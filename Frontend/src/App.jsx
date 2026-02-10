f7dcff107796cecc30202bff7ca871ff6590bb78
qhoncheh <ghonche.ataee@gmail.com>
2026-02-10 11:09:14 +0330

    deleted unused code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import {Routes , Route} from 'react-router-dom';
 import  Home  from './pages/Home';
-// import { AboutPage } from './pages/AboutPage';
-// import { ContactPage } from './pages/ContactPage';
+
 
 export const App = () => {
   return (
     <>
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/about" element={<AboutPage />} /> */}
-      {/* <Route path="/contact" element={<ContactPage />} /> */}
     </Routes>
     </>
-  )
+  ) 
 }
